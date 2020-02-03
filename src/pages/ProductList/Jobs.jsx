54f77e764a1daed2af17c0b2156690c5f8a12abe
fikri <fikri@fikri.com>
2020-02-04 00:31:33 +0700

    jobs.jsx add location and categories

--- src/pages/ProductList/Jobs.jsx
+++ src/pages/ProductList/Jobs.jsx
@@ -13,20 +13,20 @@ const lorem = new LoremIpsum({
     }
   });
 
-const DataForInfluencer = (title, datePosted,verified,description,categories,priceRange,minFollower,minEngagementRate,numOfBid) => {
+const DataForInfluencer = (title, user,datePosted,location,description,categories,priceRange,minFollower,minEngagementRate,numOfBid) => {
     return{
-        title,datePosted,verified,description,categories,priceRange,minFollower,minEngagementRate,numOfBid
+        title,user,datePosted,location,description,categories,priceRange,minFollower,minEngagementRate,numOfBid
     }
 }
 
 
 const data_for_influencer = [
-    DataForInfluencer('Dicari Cowok Ganteng','3','1',lorem.generateSentences(3),'makanan','10.000 - 15. 000',null,null,0),
-    DataForInfluencer('Ngiklanin Product Chicken Panas','10','1',lorem.generateSentences(3),'makanan','15.000 - 25. 000',2000,1,0),
-    DataForInfluencer('Saya Butuh Cewek Berhijab Cantik','1','1',lorem.generateSentences(3),'fashion','10.000 - 15. 000',1500,0.8,0),
-    DataForInfluencer(lorem.generateWords(4),'1','1',lorem.generateSentences(3),'fashion','10.000 - 15. 000',1500,0.8,0),
-    DataForInfluencer(lorem.generateWords(3),'1','1',lorem.generateSentences(3),'fashion','10.000 - 15. 000',1500,0.8,0),
-    DataForInfluencer(lorem.generateWords(5),'1','1',lorem.generateSentences(3),'fashion','10.000 - 15. 000',1500,0.8,0),
+    DataForInfluencer('Dicari Cowok Ganteng','Siapptn','3','Bandung',lorem.generateSentences(3),'makanan','10.000 - 15. 000',null,null,0),
+    DataForInfluencer('Ngiklanin Product Chicken Panas','PT . Ruangguru','10','Jakarta',lorem.generateSentences(3),'makanan','15.000 - 25. 000',2000,1,0),
+    DataForInfluencer('Saya Butuh Cewek Berhijab Cantik','Fikri Collections','1','Tangerang',lorem.generateSentences(3),'fashion','10.000 - 15. 000',1500,0.8,0),
+    DataForInfluencer(lorem.generateWords(4),lorem.generateWords(1),'1','Purwokerto',lorem.generateSentences(3),'fashion','20.000 - 25. 000',1500,0.8,0),
+    DataForInfluencer(lorem.generateWords(3),lorem.generateWords(1),'1','Cilacap',lorem.generateSentences(3),'fashion','10.000 - 15. 000',1500,0.8,0),
+    DataForInfluencer(lorem.generateWords(5),lorem.generateWords(1),'1','Semarang',lorem.generateSentences(3),'fashion','5.000 - 10. 000',1500,0.8,0),
 ]
 
 
@@ -40,8 +40,19 @@ class productList extends Component {
                             <div className='col-9'>
                                 <h6 style={{display:'inline',fontWeight:'bold'}}>{val.title}</h6> <span className='ml-2' style={{fontSize:'12px', fontWeight:'lighter',color:'#1f2836',fontStyle:'italic'}}>{val.datePosted} Hari Yang Lalu</span>
                                 <MDBCardText>
+                                    <div>
+                                        <span style={{color : '#1f2836',fontSize:12,fontStyle:'italic',textDecoration:'underline',cursor:'pointer'}}>
+                                            {val.user}
+                                        </span>
+                                        <span className='ml-2' style={{color : 'black',fontSize:12}}>
+                                            {val.location}
+                                        </span>
+                                        <MDBBtn className='ml-2' style={{padding:3,fontSize:10,textTransform:'capitalize'}} gradient="aqua">{val.categories}</MDBBtn>
+                                    </div>
                                     {val.description}
+
                                 </MDBCardText>
+                                
                             </div>
                             <div className='col-3'>
                                 <h6 style={{fontWeight:'bold'}}>
