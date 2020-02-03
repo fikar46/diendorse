5f7db3c309fba19c7aa7f327433e5908e7450e23
fikri <fikri@fikri.com>
2020-02-03 18:31:03 +0700

    init login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import {Route,Switch} from 'react-router-dom'
+import Login from './pages/Login/Login'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar />
+        <Switch>
+          <Route path='/login' component={Login} />
+        </Switch>
       </div>
     )
   }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse} from "mdbreact";
+
+class Navbar extends Component {
+state = {
+  isOpen: false
+};
+
+toggleCollapse = () => {
+  this.setState({ isOpen: !this.state.isOpen });
+}
+
+render() {
+  return (
+      <MDBNavbar color="default-color" dark expand="md" className='px-5'>
+        <MDBNavbarBrand style={{cursor:'pointer'}}>
+          <strong className="white-text">Navbar</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBNavLink to="/login">Log In</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/signup">Sign Up</MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+    );
+  }
+}
+
+export default Navbar;
