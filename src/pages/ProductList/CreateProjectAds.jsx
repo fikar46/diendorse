9432e6b0c3d7d0a72d12f4e67333e0aeb85788f6
M Zulfikar Meylendra <fikar@fikar.local>
2020-02-09 14:58:40 +0700

    create ads

--- src/pages/ProductList/CreateProjectAds.jsx
+++ src/pages/ProductList/CreateProjectAds.jsx
@@ -16,8 +16,22 @@ class CreateProjectAds extends Component {
         upload_at:"",
         location_ads:"",
         sex_ads:"",
-        loading:false
+        loading:false,
+        categoryOptions:[],
+        priceMin:0,
+        priceMax:1
       }
+    componentDidMount(){
+      this.getCategoryAds()
+    }
+    getCategoryAds=()=>{
+      Axios.get(`${koneksi}/project/category-ads`,getHeaderAuth())
+      .then((res)=>{
+        this.setState({categoryOptions:res.data})
+      }).catch((err)=>{
+        console.log(err)
+      })
+    }
     optionCategory=(n)=>{
       this.setState({category:n.target.value})
     }
@@ -38,14 +52,13 @@ class CreateProjectAds extends Component {
       var max = this.refs.max.value;
       var id_user = this.props.user.id;
       if(category == "x"){
-        var ect_category="lain lain";
+        var ect_category=this.refs.ect_category.value;
       }else{
         var ect_category=category;
       }
       var file="test.jpg";
       var estimation_ads = JSON.stringify({min,max})
       var age_ads = JSON.stringify(value)
-      var validation = [id_user,product_name,category,ect_category,file,upload_at,location_ads,sex_ads,age_ads,description,estimation_ads];
       var status;
       
       if(product_name == ""&&category==""&&ect_category==""&&file==""&&upload_at==""&&location_ads==""&&sex_ads==""&&age_ads==""&&description==""&&estimation_ads==""){  
@@ -53,27 +66,36 @@ class CreateProjectAds extends Component {
       }else{
         status = true
       }
-      if(status){
-          Axios.post(`${koneksi}/project/create-ads`,{
-            id_user,product_name,category,ect_category,file,upload_at,location_ads,sex_ads,age_ads,description,estimation_ads
-          },getHeaderAuth()).then((res)=>{
-              Swal.fire(
-                'Project disimpan',
-                'Project iklan telah berhasil di publish!',
-                'success'
-              ).then((result)=>{
-                this.setState({loading:false})
-                if(result.value){
-                  window.location="/dashboard"
-                }
-              })         
-          }).catch((err)=>{
-              console.log(err)
-              this.setState({loading:false})
-          })
-      }else{
-          alert("Form wajib diisi semua.")
-      }
+      if(this.state.priceMin>=50000){
+          if(this.state.priceMax > this.state.priceMin){
+            if(status){
+              Axios.post(`${koneksi}/project/create-ads`,{
+                id_user,product_name,category,ect_category,file,upload_at,location_ads,sex_ads,age_ads,description,estimation_ads
+              },getHeaderAuth()).then((res)=>{
+                  Swal.fire(
+                    'Project disimpan',
+                    'Project iklan telah berhasil di publish!',
+                    'success'
+                  ).then((result)=>{
+                    this.setState({loading:false})
+                    if(result.value){
+                      window.location="/dashboard"
+                    }
+                  })         
+              }).catch((err)=>{
+                  console.log(err)
+                  this.setState({loading:false})
+              })
+          }else{
+              alert("Form wajib diisi semua.")
+          }
+          }else{
+            alert("Price must be higher than min price")
+          }
+    }else{
+      alert("Price must be more than or equal Rp 50.000")
+    }
+      
     }
     renderButton=()=>{
       if(this.state.loading){
@@ -92,42 +114,64 @@ class CreateProjectAds extends Component {
         )
       }
     }
+    optionDataCategory=()=>{
+      var data = this.state.categoryOptions.map((item)=>{
+        return (
+<option value={item.id}>{item.name}</option>
+                            
+        )
+      })
+      return data;
+    }
+    openEtcCategory=()=>{
+      if(this.state.category == "x"){
+        return(
+          <div>
+            <label htmlFor="defaultFormContactNameEx" className="black-text">
+            Other Categories Name
+            </label>
+            <input type="text" id="defaultFormContactNameEx" className="form-control" ref="ect_category"/>
+            <br />
+          </div>
+        )
+      }
+    }
     render() {
         return (
             <div className=" pt-5">
                     <MDBContainer>
                             <form>
-                                <p className="h3 text-center mb-4">Buat Iklan</p>
+                                <p className="h3 text-center mb-4">Create Ads</p>
                                 <label htmlFor="defaultFormContactNameEx" className="black-text">
-                                Nama Produk
+                                    Product Name for your ads
                                 </label>
-                                <input type="text" id="defaultFormContactNameEx" className="form-control" ref="product_name"/>
+                                <input type="text" id="defaultFormContactNameEx" className="form-control" ref="product_name" placeholder="Your Product Name"/>
                                 <br />
                                 <label htmlFor="defaultFormContactEmailEx" className="black-text">
-                                Kategori
+                                Category ads
                                 </label>
                                 <br/>
                                 <select className="form-control col-3" onChange={this.optionCategory}>
-                                    <option value="">Pilih Kategori</option>
-                                    <option value="1">Makanan</option>
-                                    <option value="2">Minuman</option>
-                                    <option value="x">Lain lain</option>
+                                    <option value="">Choose Category</option>
+                                    {this.optionDataCategory()}
+                                    <option value="x">Others</option>
                                 </select>
                                 <br />
+                                {this.openEtcCategory()}
                                 <label htmlFor="defaultFormContactSubjectEx" className="black-text">
-                                Deskripsi
+                                Description ads
                                 </label>
-                                <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="9" ref="description"/>
+                                <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="9" ref="description" placeholder="Input your ads description"/>
                                 <br />
                                 <label htmlFor="defaultFormContactMessageEx" className="black-text">
-                                Upload file<br/>
+                                Upload file for support your description<br/>
                                 <small className="grey-text">(Opsional) Anda dapat mengupload file dalam bentuk gambar atau video untuk melengkapi deskripsi iklan</small>
                                 </label><br/>
                                 <div>
                                     <button className="btn btn-warning btn-sm" type="button">+</button><br/>
                                 </div>
                                 <label htmlFor="defaultFormContactMessageEx" className="black-text mt-2">
-                                Iklan akan di terbitkan di
+                                Ads will publish at
                                 </label>
                                  <div className="my-3">
                                   <div className="form-check">
@@ -145,12 +189,12 @@ class CreateProjectAds extends Component {
                                   <div className="form-check">
                                     <input className="form-check-input" type="radio" name="PlatformRadios" id="PlatformRadios3" defaultValue="platform3" onClick={()=>this.setState({upload_at:3})}/>
                                     <label className="form-check-label" htmlFor="PlatformRadios3">
-                                      Keduanya
+                                      Both
                                     </label>
                                   </div>
                                 </div>
                                 <label htmlFor="defaultFormContactMessageEx" className="black-text">
-                                Sasaran lokasi permirsa
+                                Audience location goals
                                 </label>
                                 <br/>
                                 <select className="form-control col-3" onChange={this.locationAds}>
@@ -161,31 +205,31 @@ class CreateProjectAds extends Component {
                                 </select>
                                 <br />
                                 <label htmlFor="defaultFormContactMessageEx" className="black-text">
-                                Sasaran jenis kelamin pemirsa
+                                Audience gender goal
                                 </label>
                                 <div className="my-3">
                                   <div className="form-check">
                                     <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" defaultValue="option1" onClick={()=>this.setState({sex_ads:1})}/>
                                     <label className="form-check-label" htmlFor="exampleRadios1">
-                                      Laki Laki
+                                      Men
                                     </label>
                                   </div>
                                   <div className="form-check">
                                     <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" defaultValue="option2" onClick={()=>this.setState({sex_ads:2})}/>
                                     <label className="form-check-label" htmlFor="exampleRadios2">
-                                      Perempuan
+                                      Woman
                                     </label>
                                   </div>
                                   <div className="form-check">
                                     <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" defaultValue="option3" onClick={()=>this.setState({sex_ads:3})}/>
                                     <label className="form-check-label" htmlFor="exampleRadios3">
-                                      Umum
+                                      General
                                     </label>
                                   </div>
                                 </div>
                                 <br/>
                                 <label htmlFor="defaultFormContactMessageEx" className="black-text">
-                                Sasaran umur pemirsa
+                                 Audience age goal
                                 </label>
                                 <br/>
                                 <br/>
@@ -202,14 +246,14 @@ class CreateProjectAds extends Component {
                                 <br/>
                                 <br/>
                                 <label htmlFor="defaultFormContactMessageEx" className="black-text">
-                                Estimasi Harga iklan
+                                Estimated price of advertising
                                 </label>
                                 <div className="row">
                                 <div className="input-group mb-3 col-md-4">
                                   <div className="input-group-prepend">
                                     <span className="input-group-text">Rp</span>
                                   </div>
-                                  <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" ref="min"/>
+                                  <input type="number" className="form-control" aria-label="Amount (to the nearest dollar)" ref="min" onChange={(val)=>this.setState({priceMin:val.target.value})}/>
                                   <div className="input-group-append">
                                     <span className="input-group-text">Min</span>
                                   </div>
@@ -218,7 +262,7 @@ class CreateProjectAds extends Component {
                                   <div className="input-group-prepend">
                                     <span className="input-group-text">Rp</span>
                                   </div>
-                                  <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" ref="max"/>
+                                  <input type="number" className="form-control" aria-label="Amount (to the nearest dollar)" ref="max" onChange={(val)=>this.setState({priceMax:val.target.value})}/>
                                   <div className="input-group-append">
                                     <span className="input-group-text">Max</span>
                                   </div>
