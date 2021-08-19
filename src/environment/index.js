7744b536ca9d1beed7a086f3f2e2010d5f878a61
fikar46 <mzulfikarmey@gmail.com>
2021-08-19 09:32:29 +0700

    ads

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,9 +1,9 @@
 var api;
 if(window.location.hostname == 'localhost'){
-     api = 'http://api.meylendra.com:2021';
+     api = 'https://api.meylendra.com:2021';
 }else if(window.location.hostname == "adsfluencer.siapptn.com"){
-     api = 'http://api.meylendra.com:2021';
+     api = 'https://api.meylendra.com:2021';
 }else{
-     api = 'http://api.meylendra.com:2021';
+     api = 'https://api.meylendra.com:2021';
 }
 export const koneksi = `${api}`
